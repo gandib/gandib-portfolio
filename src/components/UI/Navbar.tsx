a864ee4b15ff1d5b7cc530daae643f5574a2df7e
gandib <55914763+gandib@users.noreply.github.com>
2024-12-24 00:07:55 +0600

    Initial setup done.

---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,100 @@
+"use client";
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  NavbarMenuItem,
+} from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
+import NextLink from "next/link";
+import { siteConfig } from "@/src/config/site";
+import { Logo } from "@/src/components/icons";
+import { ThemeSwitch } from "./theme-switch";
+import { usePathname, useRouter } from "next/navigation";
+
+export const Navbar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <NextUINavbar
+      maxWidth="2xl"
+      position="sticky"
+      // className="bg-gray-600"
+      isBordered={true}
+    >
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Logo />
+            <p className="font-bold text-lg text-inherit">Gandib</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden lg:flex gap-4 justify-start items-center ml-2">
+          {/* <NavbarItem>
+            <NextLink
+              className={`text-sm font-bold ${pathname === "/recent" ? "text-primary-500" : ""}`}
+              href="/recent"
+            >
+              Recent
+            </NextLink>
+          </NavbarItem> */}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden md:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        {siteConfig.navMenuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={pathname === item.href ? "primary" : "foreground"}
+              href={item.href}
+              size="lg"
+              className="font-bold"
+            >
+              {item.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        <NavbarItem className="hidden lg:flex gap-2">
+          <ThemeSwitch />
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="lg:hidden basis-1 pl-4" justify="end">
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {/* <NavbarItem>
+            <NextLink
+              className={`text-lg ${pathname === "/recent" ? "text-primary-500" : ""}`}
+              href="/recent"
+            >
+              Recent
+            </NextLink>
+          </NavbarItem> */}
+
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={pathname === item.href ? "primary" : "foreground"}
+                href={item.href}
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </NextUINavbar>
+  );
+};
---
+++ src/config/site.ts
@@ -0,0 +1,41 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Gandib",
+  description: "Gandib Dhari Roy is a Full Stack Web Developer.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
